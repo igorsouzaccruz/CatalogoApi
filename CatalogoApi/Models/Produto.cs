f6e2b245a757686b7a13684c84a1b936d5fb6462
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2023-01-10 16:53:34 -0300

    fluent API propriedades e relacionamentos

--- CatalogoApi/Models/Produto.cs
+++ CatalogoApi/Models/Produto.cs
@@ -6,7 +6,7 @@
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
         public decimal? Preco { get; set; }
-        public string? Imageml { get; set; }
+        public string? Imagem { get; set; }
         public DateTime DataCompra { get; set; }
         public int Estoque { get; set; }
 
