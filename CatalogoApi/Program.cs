47b8e012f5afff7ec55c6485d99834ae3cefdbcc
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2023-01-11 10:49:12 -0300

    endpoints produtos

--- CatalogoApi/Program.cs
+++ CatalogoApi/Program.cs
@@ -27,19 +27,19 @@ app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 });
 
-app.MapGet("/categorias", async(AppDbContext db) => await db.Categorias.ToListAsync());
+app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync());
 
 app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
 {
     return await db.Categorias.FindAsync(id)
-        is Categoria categoria 
-        ? Results.Ok(categoria) 
+        is Categoria categoria
+        ? Results.Ok(categoria)
         : Results.NotFound();
 });
 
-app.MapPut("/categorias/{id:int}", async(int id, Categoria categoria, AppDbContext db) =>
+app.MapPut("/categorias/{id:int}", async (int id, Categoria categoria, AppDbContext db) =>
 {
-    if(categoria.CategoriaId != id)
+    if (categoria.CategoriaId != id)
     {
         return Results.BadRequest();
     }
@@ -66,6 +66,62 @@ app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
 
     return Results.NoContent();
 });
+
+//------------------------------------- endpoints para Produto ------------------------------------
+app.MapPost("/produtos", async (Produto produto, AppDbContext db) =>
+{
+    db.Produtos.Add(produto);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/produtos/{produto.ProdutoId}", produto);
+});
+
+app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
+
+app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
+{
+    return await db.Produtos.FindAsync(id)
+        is Produto produto
+        ? Results.Ok(produto)
+        : Results.NotFound();
+});
+
+app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db) =>
+{
+    if (produto.ProdutoId != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var produtoDB = await db.Produtos.FindAsync(id);
+
+    if (produtoDB is null) return Results.NotFound();
+
+    produtoDB.Nome = produto.Nome;
+    produtoDB.Descricao = produto.Descricao;
+    produtoDB.Preco = produto.Preco;
+    produtoDB.Imagem = produto.Imagem;
+    produtoDB.DataCompra = produto.DataCompra;
+    produtoDB.Estoque = produto.Estoque;
+    produtoDB.CategoriaId = produto.CategoriaId;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok(produtoDB);
+});
+
+app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
+{
+    var produto = await db.Produtos.FindAsync(id);
+
+    if (produto is null) return Results.NotFound();
+
+    db.Produtos.Remove(produto);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
 
 
 
