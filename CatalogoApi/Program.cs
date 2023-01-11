db5ce5ac298b0ce50cf0c1933472e0b820c81a59
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2023-01-11 10:08:57 -0300

    create MapGet to categorias

--- CatalogoApi/Program.cs
+++ CatalogoApi/Program.cs
@@ -1,4 +1,5 @@
 using CatalogoApi.Context;
+using CatalogoApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,30 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
+//Definir os Endpoints
+
+app.MapGet("/", () => "Catálogo de Produtos - 2023");
+
+app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
+{
+    db.Categorias.Add(categoria);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
+});
+
+app.MapGet("/categorias", async(AppDbContext db) => await db.Categorias.ToListAsync());
+
+app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    return await db.Categorias.FindAsync(id)
+        is Categoria categoria 
+        ? Results.Ok(categoria) 
+        : Results.NotFound();
+});
+
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
